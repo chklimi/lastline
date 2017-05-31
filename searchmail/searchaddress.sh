85b9a0485398a5a36f393dd6b1ef82ad54f8f912
Dirk Beste <dirk.beste@gmail.com>
2017-05-31 17:15:16 +0000

    Added comments

--- searchmail/searchaddress.sh
+++ searchmail/searchaddress.sh
@@ -1,2 +1,5 @@
+##!/usr/bin/env bash
+#
+#
 output=$(grep "$1"  /var/log/llmail/email/SmtpReceiver.0.email.log | cut -d' ' -f4)
 for x in $output; do echo; echo "###### Searching in mail addresses for '$1' and message id $x"; grep "$x" /var/log/llmail/email/* ;  done; 
